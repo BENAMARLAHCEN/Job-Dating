fb9973f5ba96eb96c689ff398097829b2758f135
lahcen <lahcen.ben3@gmail.com>
2024-01-31 11:08:14 +0100

    added Announcement controller function content with simple crud functionnality

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Announcement;
 use Illuminate\Http\Request;
 
 class AnnouncementController extends Controller
@@ -11,7 +12,8 @@ class AnnouncementController extends Controller
      */
     public function index()
     {
-        //
+        $announcements = Announcement::all();
+        return view('admin.announcements.index', compact('announcements'));
     }
 
     /**
@@ -19,7 +21,8 @@ class AnnouncementController extends Controller
      */
     public function create()
     {
-        //
+        $companies = Company::all();
+        return view('admin.announcements.create', compact('companies'));
     }
 
     /**
@@ -27,38 +30,54 @@ class AnnouncementController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Announcement::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'date' => $request->date,
+            'company_id' => $request->company_id,
+        ]);
+
+        return redirect()->route('announcements.index')->with('success', 'Announcement created successfully');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Announcement $announcement)
     {
-        //
+        return view('admin.announcements.show', compact('announcement'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Announcement $announcement)
     {
-        //
+        $companies = Company::all();
+        return view('admin.announcements.edit', compact('announcement', 'companies'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Announcement $announcement)
     {
-        //
+        $announcement->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'date' => $request->date,
+            'company_id' => $request->company_id,
+        ]);
+
+        return redirect()->route('announcements.index')->with('success', 'Announcement updated successfully');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Announcement $announcement)
     {
-        //
+        $announcement->delete();
+        return redirect()->route('announcements.index')->with('success', 'Announcement deleted successfully');
     }
 }
