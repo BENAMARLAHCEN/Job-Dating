3ddfc1ef7c58c70f5e7709b3d315123b18b91266
lahcen <lahcen.ben3@gmail.com>
2024-02-01 16:52:29 +0100

    added skills in announcement table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Announcement;
+use App\Models\Company;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        // Company::factory(10)->create();
+        Announcement::factory(40)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
