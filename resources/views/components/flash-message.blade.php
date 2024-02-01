c009def979b7d0be5a3041b6a916fa6c26e49323
lahcen <lahcen.ben3@gmail.com>
2024-02-01 16:35:34 +0100

    added errors flash message in components

---
+++ resources/views/components/flash-message.blade.php
@@ -0,0 +1,25 @@
+@props(['flash-message'])
+
+@if (session()->has('message'))
+<div id="alertMessage" class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py-3">
+    <p class="inline-block mr-4">
+        {{ session('message') }}
+    </p>
+    <button onclick="closeAlert(this)"
+        class="absolute right-4 top-1/2 transform -translate-y-1/2 text-white hover:text-gray-300">
+        &times;
+    </button>
+</div>
+<script>
+    setTimeout(function() {
+        closeAlert();
+    }, 3000);
+
+    function closeAlert() {
+        var alertMessage = document.getElementById('alertMessage');
+        if (alertMessage) {
+            alertMessage.style.display = 'none';
+        }
+    }
+</script>
+@endif
