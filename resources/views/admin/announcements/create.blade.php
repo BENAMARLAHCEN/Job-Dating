66763f7ba5b89852db0c64c23fa7bf6184536899
lahcen <lahcen.ben3@gmail.com>
2024-02-01 16:42:35 +0100

    adding create form for anouncement

---
+++ resources/views/admin/announcements/create.blade.php
@@ -0,0 +1,62 @@
+@extends('admin/layout/app')
+@section('content')
+    @if ($errors->any())
+        <div id="alert-additional-content-2"
+            class="alert alert-danger alert-dismissible border rounded-lg bg-light text-danger" role="alert">
+            <div class="flex items-center">
+                <span class="sr-only">Whoops!</span>
+                <h3 class="text-lg font-medium">There were some problems with your input.</h3>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            <div class="mt-2 mb-4 text-sm">
+                <ul class="list-unstyled">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    @endif
+
+<form action="{{ route('announcements.store') }}" method="POST" enctype="multipart/form-data">
+    @csrf
+    <div class="mb-3">
+        <label for="company" class="form-label">Company Name</label>
+        <select name="company_id" class="form-select">
+            @foreach ($companies as $company)
+                <option value="{{ $company->id }}">{{ $company->name }}</option>
+            @endforeach
+        </select>
+    </div>
+
+    <div class="mb-3">
+        <label for="title" class="form-label">Job Title</label>
+        <input type="text" class="form-control" name="title" placeholder="Example: Senior Laravel Developer">
+    </div>
+    <div class="mb-3">
+        <label for="date" class="form-label">Date</label>
+        <input type="date" class="form-control" name="date">
+    </div>
+
+    <div class="mb-3">
+        <label for="skills" class="form-label">Skills (Comma Separated)</label>
+        <input type="text" class="form-control" name="skills"
+            placeholder="Example: Laravel, Backend, Postgres, etc">
+    </div>
+
+    <div class="mb-3">
+        <label for="image" class="form-label">Image</label>
+        <input type="file" class="form-control" name="image">
+    </div>
+
+    <div class="mb-3">
+        <label for="description" class="form-label">Job Description</label>
+        <textarea class="form-control" name="description" rows="5" placeholder="Include tasks, requirements, salary, etc"></textarea>
+    </div>
+
+    <div class="mb-3">
+        <button class="btn btn-primary">Create</button>
+        <a href="/" class="btn btn-secondary ml-4">Back</a>
+    </div>
+</form>
+@endsection
