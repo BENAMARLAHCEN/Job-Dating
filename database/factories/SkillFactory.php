56044110340074e664404dc1b0f4f49c2314266c
lahcen <lahcen.ben3@gmail.com>
2024-02-02 17:04:07 +0100

    create announceCompany model and modify relashin ship between company and announce

--- database/factories/SkillFactory.php
+++ database/factories/SkillFactory.php
@@ -17,7 +17,7 @@ class SkillFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->safeColorName(),
+            'name' => fake()->unique()->name()
         ];
     }
 }
