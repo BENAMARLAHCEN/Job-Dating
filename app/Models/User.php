71697bf8b7db6d51fd890f33abcb41dc168d152f
lahcen <lahcen.ben3@gmail.com>
2024-02-05 11:54:03 +0100

    first role create test

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
+    // The User model requires this trait
+    use HasRoles;
+
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
--- database/seeders/AttendanceSeeder.php
+++ database/seeders/AttendanceSeeder.php
@@ -12,6 +12,6 @@ class AttendanceSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        
     }
 }
