359350254797c7d0b3b3a587556e314279ba52bc
lahcen <lahcen.ben3@gmail.com>
2024-02-06 14:11:48 +0100

    add image in user model and relation with roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Traits\HasPermissions;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     // The User model requires this trait
-    use HasRoles;
+    use HasRoles, HasPermissions;
 
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -25,6 +28,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'image',
     ];
 
     /**
@@ -51,4 +55,5 @@ class User extends Authenticatable
     {
         return $this->hasMany(Attendance::class);
     }
+    
 }
