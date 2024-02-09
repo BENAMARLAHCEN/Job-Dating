4d3c5ff83d0caeca08754ec455b82b122be89db2
lahcen <lahcen.ben3@gmail.com>
2024-02-09 12:11:35 +0100

    create profile model and migrate

---
+++ database/migrations/2024_02_07_154401_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->unique();
+            $table->text('bio')->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('mobile')->nullable();
+            $table->string('location')->nullable();
+            $table->string('job')->nullable();
+            $table->string('address')->nullable();
+            $table->string('linkedin')->nullable();
+            $table->string('github')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('instagram')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
